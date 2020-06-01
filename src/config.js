8b4540cb7cf902de94988a910b0352a1de428066
TK <taehnkim@gmail.com>
2020-05-31 17:28:54 -0700

    Add favicon, routing, home+404 pages, amplify config

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-uploads-tae"
+  },
+  apiGateway: {
+    REGION: "us-west-2",
+    URL: "https://h6k29ognoi.execute-api.us-west-2.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-west-2",
+    USER_POOL_ID: "us-west-2_lbtLbDc00",
+    APP_CLIENT_ID: "2jfndpki780o610qa3eg831g69",
+    IDENTITY_POOL_ID: "us-west-2:c2d72089-6335-4a80-bf33-7995e3d97035"
+  }
+};
---
+++ src/containers/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './NotFound.css';
+
+export const NotFound = () => {
+  return (
+    <div className="NotFound">
+      <h3>Sorry, page not found!</h3>
+    </div>
+  );
+}
